3d0463a7b7a97b21be7aa9cb516fff0f2343a767
Fabian Espinosa <fabianespinosa1988@gmail.com>
2023-03-06 04:13:25 +0100

    formats

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -31,16 +31,16 @@ function Banner({ netflixOriginals }: Props) {
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
 
-      <p className="max-w-xs text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
 
-      <div className='flex space-x-3'>
+      <div className="flex space-x-3">
         <button className="bannerButton bg-white text-black">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"></FaPlay> Play
         </button>
         <button className="bannerButton bg-[gray]/70">
-          More Info <InformationCircleIcon className='h-5 w-8 md:h-8 md:w-8'/>
+          More Info <InformationCircleIcon className="h-5 w-8 md:h-8 md:w-8" />
         </button>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home = ({
   comedyMovies,
   horrorMovies,
   romanceMovies,
-  documentaries,
+  documentaries
 }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -62,7 +62,7 @@ export const getServerSideProps = async () => {
     comedyMovies,
     horrorMovies,
     romanceMovies,
-    documentaries,
+    documentaries
   ] = await Promise.all([
     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
     fetch(requests.fetchTrending).then((res) => res.json()),
@@ -71,7 +71,7 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchComedyMovies).then((res) => res.json()),
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json())
   ])
 
   return {
@@ -83,7 +83,7 @@ export const getServerSideProps = async () => {
       comedyMovies: comedyMovies.results,
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
-    },
+      documentaries: documentaries.results
+    }
   }
 }
